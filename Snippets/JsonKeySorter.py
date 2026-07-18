1989658acbe844b89e38aef3d51b632d8d80f4ed
Akshit <ak.bisht1@gmail.com>
2026-07-18 09:31:58 +0530

    Add sorting

---
+++ Snippets/JsonKeySorter.py
@@ -0,0 +1,11 @@
+import json
+
+file_path = "media.json"
+
+# 1. Read the original data
+with open(file_path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+# 2. Overwrite the original file with sorted keys
+with open(file_path, "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=4, sort_keys=True)
